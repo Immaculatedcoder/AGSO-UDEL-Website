5d9b2530f556c5fed62c6e8d7126473e3b80c2b4
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2025-12-19 21:42:34 -0500

    Footer

--- african-grad-org/src/components/Footer/Footer.jsx
+++ african-grad-org/src/components/Footer/Footer.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 
 function Footer() {
-    
+    return (
+        <footer>
+            <div>
+                {/* Top section */}
+                <div>
+                    {/* About */}
+                    <div>
+                        <h3> About AGSO</h3>
+                        <p> 
+                            The African Graduate Student Organization at the University of 
+                            Delaware fosters community, celebrates African culture, and support 
+                            graduate students.
+                        </p>
+                    </div>
+
+                    {/* Quick Links */}
+                    <div>
+                        <h3> Quick Links</h3>
+                        <ul>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 }
+
+export default Footer
