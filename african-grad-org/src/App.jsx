86229bd3524acfbdf57e7c7e29b5c2277f016c25
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2025-12-27 15:15:50 -0500

    Resturcture the code

--- african-grad-org/src/App.jsx
+++ african-grad-org/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import Index from "./pages/Index";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Index />} />
       </Routes>
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
--- african-grad-org/src/pages/Index.jsx
+++ african-grad-org/src/pages/Index.jsx
@@ -1,15 +1,11 @@
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
 function Index() {
   return (
     <div>
-      <Header />
       <main>
         {/* <HeroSection /> */}
         {/* Section 2 */}
         {/* Section 3 */}
       </main>
-      {/* <Footer /> */}
     </div>
   );
 }
