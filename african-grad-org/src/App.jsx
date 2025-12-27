52251da5b159802967c767ff2c1844d75c7651b5
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2025-12-27 17:38:02 -0500

    updated the footer

--- african-grad-org/src/App.jsx
+++ african-grad-org/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Index />} />
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </BrowserRouter>
   );
 }
--- african-grad-org/src/components/Footer/Footer.jsx
+++ african-grad-org/src/components/Footer/Footer.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import { Link, NavLink } from "react-router-dom";
+import "./Footer.css";
 
 function Footer() {
   const currentYear = new Date().getFullYear();
 
   const quickLinks = [
-    { id: 1, name: "Home", href: "/" },
+    // { id: 1, name: "Home", href: "/" },
     { id: 2, name: "About Us", href: "/about" },
     { id: 3, name: "Leadership", href: "/leadership" },
     { id: 4, name: "Events", href: "/events" },
   ];
 
   return (
-    <footer>
-      <div>
+    <footer className="footer">
+      <div className="footer__container">
         {/* Top section */}
-        <div>
+        <div className="footer__container-top">
           {/* About */}
-          <div>
+          <div className="footer-about__container">
             <h3> About AGSO</h3>
             <p>
               The African Graduate Student Organization at the University of
@@ -28,41 +30,48 @@ function Footer() {
           </div>
 
           {/* Quick Links */}
-          <div>
+          <div className="footer-quickLinks__container">
             <h3> Quick Links</h3>
-            <ul>
+            <nav className="footer-quickLinks_subcontainer">
+              <Link to="/" className="footer-quickLinks__nav-link">
+                Home
+              </Link>
               {quickLinks.map((links) => {
                 return (
-                  <li key={links.id}>
-                    <a href={links.href}> {links.name}</a>
-                  </li>
+                  <NavLink
+                    key={links.id}
+                    to={links.href}
+                    className="footer-quickLinks__nav-link"
+                  >
+                    {links.name}
+                  </NavLink>
                 );
               })}
-            </ul>
+            </nav>
           </div>
 
           {/* Connect */}
-          <div>
+          <div className="footer-connect__container">
             <h3> Connect With Us</h3>
-            <div>
+            <div className="footer__icons">
               <a href="#">
-                {" "}
                 <InstagramIcon />
               </a>
               <a href="#">
-                {" "}
                 <WhatsAppIcon />
               </a>
             </div>
           </div>
+        </div>
 
-          {/* Divider */}
-          <div>
-            <p>
-              © {currentYear} African Graduate Student Organization. All rights
-              reserved.
-            </p>
-          </div>
+        {/* Divider */}
+        <div className="footer__container-divider"></div>
+
+        <div className="footer__container-buttom">
+          <p>
+            © {currentYear} African Graduate Student Organization. All rights
+            reserved.
+          </p>
         </div>
       </div>
     </footer>
