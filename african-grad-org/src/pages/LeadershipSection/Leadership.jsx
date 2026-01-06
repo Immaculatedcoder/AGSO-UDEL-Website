b03a56cfc2270bdfd64fe573c0cf7030046bbd78
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2026-01-05 23:38:27 -0500

    Commit

--- african-grad-org/src/pages/LeadershipSection/Leadership.jsx
+++ african-grad-org/src/pages/LeadershipSection/Leadership.jsx
@@ -1,9 +1,28 @@
 import "./Leadership.css";
 
+const LeadersProfile = [
+  {
+    id: 1,
+    name: "Emmanuel Adebayo",
+  },
+];
+
 function Leadership() {
   return (
     <section className="LeadershipSection">
-      <div></div>
+      <div className="leadership__container">
+        {/* Top part if the leadership section */}
+        <div className="leadership__header">
+          <h2 className="leadership__title">Our Leadership</h2>
+          <p className="leadership__description">
+            Meet the dedicated team working to make AGSO a virbrant and
+            supportive community for all African graduate students.
+          </p>
+        </div>
+
+        {/* Down parts: Leaders card */}
+        <div className="leaders__container"></div>
+      </div>
     </section>
   );
 }
